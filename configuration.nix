aa1e0215cfa83507eae8eeccbbbe7c9c5b762ed5
José Tobias <jose.tobias@outlook.com>
2023-12-23 21:03:57 -0300

    First commit
    Signed-off-by: José Tobias <jose.tobias@outlook.com>

---
+++ configuration.nix
@@ -0,0 +1,191 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }: {
+  imports = [ 
+    ./hardware-configuration.nix
+  ];
+
+  # boot - loader
+  boot.loader.grub = {
+    enable = true;
+    useOSProber = true;
+    device = "/dev/vda";
+  };
+
+  # boot - kernel modules
+  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+    };
+  };
+
+  # Some programs might need it to configure themselves
+  programs.dconf.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keyboard
+  services.xserver = {
+    layout = "br";
+    xkbVariant = "";
+  };
+  services.xserver.autoRepeatDelay = 150;
+  services.xserver.autoRepeatInterval = 25;
+
+  # Configure console keymap
+  console.keyMap = "br-abnt2";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable flatpak.
+  services.flatpak.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Allow experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Virtualisation
+  virtualisation = {
+    docker = {
+      enable = true;
+      # only if you're using btrfs!
+      # storageDriver = "btrfs";
+    };
+
+    libvirtd.enable = true;
+  };
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    tmux
+    home-manager
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # NVIDIA
+  # # Load nvidia driver for Xorg and Wayland
+  # services.xserver.videoDrivers = ["nvidia"];
+
+  # hardware.nvidia = {
+  #   # Modesetting is required.
+  #   modesetting.enable = true;
+
+  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  #   powerManagement.enable = false;
+
+  #   # Fine-grained power management. Turns off GPU when not in use.
+  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  #   powerManagement.finegrained = false;
+
+  #   # Use the NVidia open source kernel module (not to be confused with the
+  #   # independent third-party "nouveau" open source driver).
+  #   # Support is limited to the Turing and later architectures. Full list of 
+  #   # supported GPUs is at: 
+  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+  #   # Only available from driver 515.43.04+
+  #   # Currently alpha-quality/buggy, so false is currently the recommended setting.
+  #   open = false;
+
+  #   # Enable the Nvidia settings menu,
+	#   # accessible via `nvidia-settings`.
+  #   nvidiaSettings = true;
+
+  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
+
+  # Define MY user account. Don't forget to set a password with ‘passwd’.
+  users.users.jtobias = {
+    isNormalUser = true;
+    description = "José Tobias";
+    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" ];
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
---
+++ home.nix
@@ -0,0 +1,105 @@
+{ pkgs, ... }: {
+  # Allow unfree
+  nixpkgs.config.allowUnfree = true;
+
+  fonts.fontconfig.enable = true;
+
+  home = {
+    username = "jtobias";
+    homeDirectory = "/home/jtobias";
+
+    packages = with pkgs; [
+      # Browsers
+      firefox
+      google-chrome
+      opera
+
+      # Mail
+      thunderbird
+
+      # Editors
+      neovim
+      emacs
+
+      virt-manager
+      dconf
+
+      # Code Management
+      git
+      lazygit
+
+      # Development
+      jetbrains-toolbox
+      minikube
+      # java
+      gradle
+      gradle-completion
+      jdk
+      ant
+        
+      # zig
+      zig
+      zig-shell-completions
+
+      # Shell
+      fastfetch
+      nerdfonts
+
+      # General
+      vscode
+
+      # Utils
+      aria
+      zip
+      p7zip
+    ];
+  };
+
+  # Programs configurations
+  programs = {
+    home-manager.enable = true;
+    bash.enable = true;
+    
+    
+    starship = {
+      enable = true;
+      
+      settings = {
+        add_newline = true;
+        
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[➜](bold red)";
+        };
+
+        cmd_duration = {
+          disabled = false;
+          min_time = 0;
+        };
+
+        # os = {
+        #   disabled = false;
+        # };
+      };
+    };
+
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv = {
+        enable = true;
+      };
+    };
+  };
+
+  # Dconf configuration to save virt-manager configs
+  dconf.settings = {
+    "org/virt-manager/virt-manager/connections" = {
+    autoconnect = ["qemu:///system"];
+    uris = ["qemu:///system"];
+    };
+  };
+
+  /* The home.stateVersion option does not have a default and must be set */
+  home.stateVersion = "23.11";
+}
