c265b141eb1ed564666ccf8bea79049dd5973f5c
José Tobias <jose.tobias@outlook.com>
2023-12-31 06:46:06 -0300

    feat: properly setup apache-kafka service to run with zookeper service

--- imports/services/kafka/default.nix
+++ imports/services/kafka/default.nix
@@ -1,23 +1,25 @@
 { config, pkgs, ... }: {
-  # Zookeeper
-  services.zookeeper = {
-    enable = true;
+  services = {
+    # Zookeeper
+    zookeeper.enable = true;
+
+    # Kafka
+    apache-kafka = {
+      enable = true;
+      settings = {
+        "log.dirs" = [ "/var/log/apache-kafka/" ];
+        "zookeeper.connect" = "localhost:" + builtins.toString config.services.zookeeper.port;
+      };
+    };
   };
 
-  # Kafka
-  services.apache-kafka = {
-    enable = true;
-
-    settings = {
-      "log.dirs" = [ "/var/log/apache-kafka/" ];
-      "zookeeper.connect" = "localhost:" + builtins.toString config.services.zookeeper.port;
-    };
+  systemd.services."apache-kafka" = {
+    requires = [ "zookeeper.service" ];
+    after = [ "zookeeper.service" ];
   };
 
   # Just because the Kafka service doesn't provide the scripts, I'm going
   # to install something to make it useful - not my choice, may be gone in
   # the near future
-  environment.systemPackages = with pkgs; [
-    kafkactl
-  ];
+  environment.systemPackages = with pkgs; [ kafkactl ];
 }
