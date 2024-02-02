d21f304a60314bcba32a0e9418ba86cf1fd359bc
José Tobias <jose.tobias@outlook.com>
2024-02-02 14:34:42 -0300

    feat: limit spark worker resources

--- configuration/system/services/spark/default.nix
+++ configuration/system/services/spark/default.nix
@@ -10,6 +10,10 @@
     worker = {
       enable = true;
       master = "127.0.0.1:7077";
+      extraEnvironment = {
+        SPARK_WORKER_CORES = 2;
+        SPARK_WORKER_MEMORY = "2g";
+      }
     };
   };
 }
