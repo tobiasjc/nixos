77b55db41af5b8442c5589912358be9fda907a1b
José Tobias <jose.tobias@outlook.com>
2024-02-02 21:32:07 -0300

    fix: starship configuration

--- configuration/home-manager/programs/starship/default.nix
+++ configuration/home-manager/programs/starship/default.nix
@@ -7,13 +7,13 @@
 
       character = {
         format = "$symbol ";
-        success_symbol = "[\\$](bold green)";
-        error_symbol = "[\\$](bold red)";
+        success_symbol = "[ \\$](bold green)";
+        error_symbol = "[ \\$](bold red)";
 
-        vimcmd_symbol = "[\\$](bold green)";
-        vimcmd_replace_one_symbol = "[\\$](bold cyan)";
-        vimcmd_replace_symbol = "[\\$](bold purple)";
-        vimcmd_visual_symbol = "[\\$](bold yellow)";
+        vimcmd_symbol = "[ \\$](bold green)";
+        vimcmd_replace_one_symbol = "[ \\$](bold cyan)";
+        vimcmd_replace_symbol = "[ \\$](bold purple)";
+        vimcmd_visual_symbol = "[ \\$](bold yellow)";
         disabled = false;
       };
 
@@ -38,7 +38,7 @@
       };
 
       os = {
-        disabled = true;
+        disabled = false;
       };
     };
   };
