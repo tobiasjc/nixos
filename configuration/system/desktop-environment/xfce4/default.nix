708954f0cac11a25c512a841743d4fa3ff9379c7
José Tobias <jose.tobias@outlook.com>
2024-02-03 00:33:34 -0300

    feat: add xfce plugins in the best way known to man right now

--- configuration/system/desktop-environment/xfce4/default.nix
+++ configuration/system/desktop-environment/xfce4/default.nix
@@ -16,4 +16,11 @@
       thunar-volman
     ];
   };
+
+  environment.systemPackages = with pkgs.xfce; [
+    xfce4-pulseaudio-plugin
+    xfce4-systemload-plugin
+    xfce4-cpugraph-plugin
+    xfce4-netload-plugin
+  ];
 }
