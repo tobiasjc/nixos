bf08877caea589b3321c99ce1745ba2e5f26c5ae
José Tobias <jose.tobias@outlook.com>
2024-03-05 00:51:42 -0300

    feat: add sbt for scala, ninja and cmake for c/c++ build

--- configuration/home-manager/packages/development/build/default.nix
+++ configuration/home-manager/packages/development/build/default.nix
@@ -1,9 +1,17 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [
+    # c/c++
     gnumake
+    cmake
+    ninja
+
+    # java/jvm in general
     gradle
     gradle-completion
     maven
     ant
+
+    # scala
+    sbt
   ];
 }
