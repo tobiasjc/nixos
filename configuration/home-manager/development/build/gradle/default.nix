488de4e7ef0e36ecce7bc9ae4cdb584ce9d28a93
José Tobias <jose.tobias@outlook.com>
2024-07-18 21:59:26 -0300

    feat: gradle configs

--- configuration/home-manager/development/build/gradle/default.nix
+++ configuration/home-manager/development/build/gradle/default.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }: {
   programs.gradle = {
     enable = true;
-    settings = { };
+    settings = {
+      "org.gradle.caching" = true;
+      "org.gradle.parallel" = true;
+      "org.gradle.daemon" = true;
+    };
   };
 }
