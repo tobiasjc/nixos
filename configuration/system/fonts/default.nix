335eea41b474eb83fcf53f2aaff1400ad4b47136
José Tobias <jose.tobias@outlook.com>
2026-02-22 13:55:29 -0300

    feat: nerd-fonts now need to be specified individually

--- configuration/system/fonts/default.nix
+++ configuration/system/fonts/default.nix
@@ -2,7 +2,34 @@
 {
   fonts = {
     packages = with pkgs; [
-      nerdfonts
+      nerd-fonts."m+"
+      nerd-fonts.noto
+      nerd-fonts.hack
+      nerd-fonts.agave
+      nerd-fonts.monoid
+      nerd-fonts.profont
+      nerd-fonts.monofur
+      nerd-fonts.hasklug
+      nerd-fonts.cousine
+      nerd-fonts.go-mono
+      nerd-fonts.overpass
+      nerd-fonts.mononoki
+      nerd-fonts.meslo-lg
+      nerd-fonts.monaspace
+      nerd-fonts.fira-mono
+      nerd-fonts.fira-code
+      nerd-fonts.anonymice
+      nerd-fonts.space-mono
+      nerd-fonts.ubuntu-sans
+      nerd-fonts.ubuntu-mono
+      nerd-fonts.intone-mono
+      nerd-fonts.inconsolata
+      nerd-fonts.commit-mono
+      nerd-fonts.symbols-only
+      nerd-fonts.iosevka-term
+      nerd-fonts.adwaita-mono
+      nerd-fonts.atkynson-mono
+
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
