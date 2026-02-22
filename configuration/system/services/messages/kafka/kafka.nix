cc4867f332ac6b18503a27fe71eda711f441825e
José Tobias <jose.tobias@outlook.com>
2026-02-22 19:29:37 -0300

    feat: apache-kafka configuration

--- configuration/system/services/messages/kafka/kafka.nix
+++ configuration/system/services/messages/kafka/kafka.nix
@@ -3,8 +3,10 @@
   services.apache-kafka = {
     enable = true;
     settings = {
-      "log.dirs" = [ "/var/log/apache-kafka/" ];
+      "node.id" = "main";
+      "listeners" = "PLAINTEXT://:9092";
       "process.roles" = "broker,controller";
+      "log.dirs" = [ "/var/log/apache-kafka/" ];
     };
   };
 
