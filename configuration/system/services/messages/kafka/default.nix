77a655767f3aa113b36729683e92345be0a93cb8
José Tobias <jose.tobias@outlook.com>
2026-02-22 19:32:56 -0300

    fix: kafka service filename

--- configuration/system/services/messages/kafka/default.nix
+++ configuration/system/services/messages/kafka/default.nix
@@ -4,7 +4,7 @@
     enable = true;
     settings = {
       "node.id" = "main";
-      "listeners" = "PLAINTEXT://:9092";
+      "listeners" = [ "PLAINTEXT://:9092" ];
       "process.roles" = "broker,controller";
       "log.dirs" = [ "/var/log/apache-kafka/" ];
     };
